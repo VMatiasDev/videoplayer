65f45c38ac4117c4ad58fd109be9ff95e1a5d5d1
Vitor Matias <vitormatiasdev@gmail.com>
2022-12-23 20:30:00 -0300

    docs: adds description on videos mock

--- components/data.js
+++ components/data.js
@@ -5,7 +5,9 @@ const Data = [
       'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/images%2Fwolf1.jpg?alt=media&token=a7c00467-40d4-43fe-9a98-6a3bebb221c7',
     videoSrc:
       'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/videos%2Fwolf1.mp4?alt=media&token=f64bbd23-4ddf-4b8c-a068-5305d164f95c',
-    videoName: 'Wolf looking to you',
+    videoName: 'Lobo olhando para você',
+    description:
+      'O lobo (também chamado de lobo-cinzento; nome científico: Canis lupus) é uma espécie de mamífero canídeo do gênero Canis. É um sobrevivente da Era do Gelo, originário do Pleistoceno Superior, cerca de 300 mil anos atrás. É o maior membro remanescente selvagem da família canidae.',
   },
   {
     id: 2,
@@ -13,7 +15,9 @@ const Data = [
       'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/images%2Fwolf2.jpg?alt=media&token=38e9b080-fc01-461c-819d-2a8d2fd01696',
     videoSrc:
       'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/videos%2Fwolf2.mp4?alt=media&token=a66d46ba-18b0-4e77-ae57-2877b4fb9f6e',
-    videoName: 'Wolf relaxing',
+    videoName: 'Lobo relaxando',
+    description:
+      'Uma série de outras subespécies do lobo-cinzento foram identificadas, embora o número real de subespécies ainda esteja em discussão. Os lobos-cinzentos são tipicamente predadores ápice nos ecossistemas que ocupam. Embora não sejam tão adaptáveis à presença humana como geralmente ocorre com as demais espécies de canídeos,[3] os lobos se desenvolveram em diversos ambientes, como florestas temperadas, desertos, montanhas, tundras, taigas, campos e até mesmo em algumas áreas urbanas.',
   },
   {
     id: 3,
@@ -21,7 +25,9 @@ const Data = [
       'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/images%2Flion1.jpg?alt=media&token=adb41a26-4488-4832-a4d4-89c788c66b6b',
     videoSrc:
       'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/videos%2Flion1.mp4?alt=media&token=2c475150-5842-422c-9dca-89ddf3be869e',
-    videoName: 'Lion family',
+    videoName: 'Família de leões',
+    description:
+      'O leão[3] [feminino: leoa] (nome científico: Panthera leo) é uma espécie de mamífero carnívoro do gênero Panthera e da família Felidae. A espécie é atualmente encontrada na África subsaariana e na Ásia, com uma única população remanescente em perigo, no Parque Nacional da Floresta de Gir, Gujarat, Índia. Foi extinto na África do Norte e no Sudoeste Asiático em tempos históricos, e até o Pleistoceno Superior, há cerca de 10 000 anos, era o mais difundido grande mamífero terrestre depois dos humanos, sendo encontrado na maior parte da África, em muito da Eurásia, da Europa Ocidental à Índia, e na América, do Yukon ao México. É uma dos quatro grandes felinos, com alguns machos excedendo 250 quilogramas em peso, sendo o segundo maior felino recente depois do tigre.',
   },
   {
     id: 4,
@@ -29,7 +35,9 @@ const Data = [
       'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/images%2Flion2.jpg?alt=media&token=b3e7d1d5-dbaa-4318-8453-551190835588',
     videoSrc:
       'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/videos%2Flion2.mp4?alt=media&token=21982cc0-d9f2-44ec-b10e-3c037b4c80ec',
-    videoName: 'Lion close',
+    videoName: 'Leão perto',
+    description:
+      'A pelagem é unicolor de coloração castanha, e os machos apresentam uma juba característica. Uma das características mais marcantes da espécie é a presença de um tufo de pelos pretos na cauda, que também possui uma espora. Habita preferencialmente as savanas e pastagens abertas, mas pode ser encontrado em regiões mais arbustivas. É um animal sociável que vive em grupos que consiste das leoas e suas crias, o macho dominante e alguns machos jovens que ainda não alcançaram a maturidade sexual. A dieta consiste principalmente de grandes ungulados e possuem hábitos noturnos e crepusculares, descansando e dormindo na maior parte do dia. Leões vivem por volta de 10-14 anos na natureza, enquanto em cativeiro eles podem viver por até 30 anos. Alguns animais desenvolveram o hábito de atacar e devorar humanos, ficando conhecidos como "devoradores de homens".',
   },
   {
     id: 5,
@@ -37,7 +45,9 @@ const Data = [
       'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/images%2Fdog1.jpg?alt=media&token=71f2aeb4-f9c2-4dd9-9d3e-58e52eae34da',
     videoSrc:
       'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/videos%2Fdog1.mp4?alt=media&token=3500d207-6925-48ac-bc23-9319a99c849f',
-    videoName: 'German Shepard relaxing',
+    videoName: 'Pastor Alemão relaxando',
+    description:
+      'Pastor-alemão (em alemão: Deutscher Schäferhund) ou lobo-da-alsácia [a][b] é uma raça canina proveniente da Alemanha.[2] Em sua origem era utilizado como cão de pastoreio de rebanhos. Atualmente é mais utilizado como cão de guarda e cão policial (K9).[3]',
   },
   {
     id: 6,
@@ -45,7 +55,9 @@ const Data = [
       'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/images%2Fdog2.jpg?alt=media&token=7f9f3540-8041-4ddc-a7f5-46c98e292e6e',
     videoSrc:
       'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/videos%2Fdog2.mp4?alt=media&token=f2c87aae-0576-45ab-8028-3247c5866a49',
-    videoName: 'German Shepard winter',
+    videoName: 'Pastor Alemão no gelo',
+    description:
+      'Na Alemanha durante o século XIX existiam três variedades de cães de pastoreio na região da Suábia, Turíngia e Württemberg, respectivamente.[4][3][5] Segundo relatos, estes cães seriam híbridos de lobos com cães trazidos a estas regiões. Especula-se ainda um possível parentesco com a raça Pastor-da-boêmia, que é bastante antiga, mas não há fatos que comprovem esta relação.[6]',
   },
   {
     id: 7,
@@ -53,7 +65,9 @@ const Data = [
       'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/images%2Fcat1.jpg?alt=media&token=fe02b285-d1a9-46c9-a6ad-f5265845114a',
     videoSrc:
       'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/videos%2Fcat1.mp4?alt=media&token=309590c7-3a9f-4a3d-b2bd-09b9b954be69',
-    videoName: 'Black cat looking to you',
+    videoName: 'Gato preto olhando para você',
+    description:
+      'O gato (nome científico: Felis silvestris catus) ou gato doméstico é um mamífero carnívoro da família dos felídeos, muito popular como animal de estimação. Ocupando o topo da cadeia alimentar, é predador natural de diversos animais, como roedores, pássaros, lagartixas e alguns insetos. Segundo pesquisas realizadas por instituições norte-americanas, os gatos consistem no segundo animal de estimação mais popular do mundo, estando numericamente atrás apenas dos peixes de aquário.[4][5] Consta em trigésimo nono na lista das 100 das espécies exóticas invasoras mais daninhas do mundo da União Internacional para a Conservação da Natureza (UICN).[6]',
   },
   {
     id: 8,
@@ -61,7 +75,9 @@ const Data = [
       'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/images%2Fcat2.jpg?alt=media&token=2ea369f8-123e-41a5-9a58-18b6dc7ee65a',
     videoSrc:
       'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/videos%2Fcat2.mp4?alt=media&token=1a837fa6-8758-46dd-aa4f-743b0308f71a',
-    videoName: 'Black cat eating',
+    videoName: 'Gato preto comendo',
+    description:
+      'A primeira associação dos gatos com os humanos da qual se tem evidência ocorreu há cerca de 9 500 anos,[7] período mais antigo ao estimado anteriormente, que oscilava entre 3 500 e 8 000 anos. A subfamília dos felíneos (Felinae), que agrupa os gatos domésticos, surgiu há cerca de 12 milhões de anos, expandindo-se a partir da África subsaariana até alcançar as terras do atual Egito.[8] Acredita-se que o gato-selvagem-africano (Felis silvestris lybica) era seu antepassado imediato.[9] Adicionalmente, evidências genéticas assinalam que os gatos domésticos atuais partilham procedência direta com os gatos selvagens do Oriente Médio.',
   },
   {
     id: 9,
@@ -69,7 +85,9 @@ const Data = [
       'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/images%2Fmonkey1.jpg?alt=media&token=e6ce978c-d92e-4c51-a585-32376951913c',
     videoSrc:
       'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/videos%2Fmonkey1.mp4?alt=media&token=b34532b9-5700-4d03-9e19-71127b831243',
-    videoName: 'Monkey eating',
+    videoName: 'Macaco comendo',
+    description:
+      'Macaco é um termo de origem africana (provavelmente do banto makako)[1] utilizado como designação comum a todas as espécies de símios ou primatas antropoides. É aplicada restritivamente no Brasil aos cebídeos (ou macacos do Novo Mundo) em geral. No sentido estrito, "macaco" refere-se às espécies de primatas pertencentes ao género Macaca.',
   },
   {
     id: 10,
@@ -77,7 +95,9 @@ const Data = [
       'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/images%2Fmonkey2.jpg?alt=media&token=874312ab-e80a-4932-997f-3b0e1baf1cdd',
     videoSrc:
       'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/videos%2Fmonkey2.mp4?alt=media&token=33a3fbaa-297c-48bf-965b-901cc8d131a2',
-    videoName: 'Monkey family',
+    videoName: 'Família de macacos',
+    description:
+      'Os macacos variam em tamanho desde o sagui-pigmeu, que pode ser tão pequeno quanto 117 mm ( 4+5 ⁄ 8  pol) com 172 mm ( 6+3 ⁄ 4  pol) de cauda e pouco mais de 100 g ( 3+1 ⁄ 2  oz) em peso,[5] para o mandril macho, quase 1 m (3 pés 3 pol) de comprimento e pesando até 36 kg (79 lb).[6]  Alguns são arbóreos (vivendo em árvores), enquanto outros vivem na savana ; as dietas diferem entre as várias espécies, mas podem conter qualquer um dos seguintes alimentos: frutas, folhas, sementes, nozes, flores, ovos e pequenos animais (incluindo insetos e aranhas).[7]',
   },
 ];
 
