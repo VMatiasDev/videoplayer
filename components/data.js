0a1b6f4038d402c3e97ecebc56018e2f7881f989
Vitor Matias <vitormatiasdev@gmail.com>
2022-12-23 19:39:47 -0300

    feat: adds mock data from firebase

---
+++ components/data.js
@@ -0,0 +1,84 @@
+const Data = [
+  {
+    id: 1,
+    imgSrc:
+      'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/images%2Fwolf1.jpg?alt=media&token=a7c00467-40d4-43fe-9a98-6a3bebb221c7',
+    videoSrc:
+      'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/videos%2Fwolf1.mp4?alt=media&token=f64bbd23-4ddf-4b8c-a068-5305d164f95c',
+    videoName: 'Wolf looking to you',
+  },
+  {
+    id: 2,
+    imgSrc:
+      'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/images%2Fwolf2.jpg?alt=media&token=38e9b080-fc01-461c-819d-2a8d2fd01696',
+    videoSrc:
+      'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/videos%2Fwolf2.mp4?alt=media&token=a66d46ba-18b0-4e77-ae57-2877b4fb9f6e',
+    videoName: 'Wolf relaxing',
+  },
+  {
+    id: 3,
+    imgSrc:
+      'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/images%2Flion1.jpg?alt=media&token=adb41a26-4488-4832-a4d4-89c788c66b6b',
+    videoSrc:
+      'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/videos%2Flion1.mp4?alt=media&token=2c475150-5842-422c-9dca-89ddf3be869e',
+    videoName: 'Lion family',
+  },
+  {
+    id: 4,
+    imgSrc:
+      'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/images%2Flion2.jpg?alt=media&token=b3e7d1d5-dbaa-4318-8453-551190835588',
+    videoSrc:
+      'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/videos%2Flion2.mp4?alt=media&token=21982cc0-d9f2-44ec-b10e-3c037b4c80ec',
+    videoName: 'Lion close',
+  },
+  {
+    id: 5,
+    imgSrc:
+      'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/images%2Fdog1.jpg?alt=media&token=71f2aeb4-f9c2-4dd9-9d3e-58e52eae34da',
+    videoSrc:
+      'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/videos%2Fdog1.mp4?alt=media&token=3500d207-6925-48ac-bc23-9319a99c849f',
+    videoName: 'German Shepard relaxing',
+  },
+  {
+    id: 6,
+    imgSrc:
+      'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/images%2Fdog2.jpg?alt=media&token=7f9f3540-8041-4ddc-a7f5-46c98e292e6e',
+    videoSrc:
+      'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/videos%2Fdog2.mp4?alt=media&token=f2c87aae-0576-45ab-8028-3247c5866a49',
+    videoName: 'German Shepard winter',
+  },
+  {
+    id: 7,
+    imgSrc:
+      'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/images%2Fcat1.jpg?alt=media&token=fe02b285-d1a9-46c9-a6ad-f5265845114a',
+    videoSrc:
+      'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/videos%2Fcat1.mp4?alt=media&token=309590c7-3a9f-4a3d-b2bd-09b9b954be69',
+    videoName: 'Black cat looking to you',
+  },
+  {
+    id: 8,
+    imgSrc:
+      'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/images%2Fcat2.jpg?alt=media&token=2ea369f8-123e-41a5-9a58-18b6dc7ee65a',
+    videoSrc:
+      'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/videos%2Fcat2.mp4?alt=media&token=1a837fa6-8758-46dd-aa4f-743b0308f71a',
+    videoName: 'Black cat eating',
+  },
+  {
+    id: 9,
+    imgSrc:
+      'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/images%2Fmonkey1.jpg?alt=media&token=e6ce978c-d92e-4c51-a585-32376951913c',
+    videoSrc:
+      'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/videos%2Fmonkey1.mp4?alt=media&token=b34532b9-5700-4d03-9e19-71127b831243',
+    videoName: 'Monkey eating',
+  },
+  {
+    id: 10,
+    imgSrc:
+      'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/images%2Fmonkey2.jpg?alt=media&token=874312ab-e80a-4932-997f-3b0e1baf1cdd',
+    videoSrc:
+      'https://firebasestorage.googleapis.com/v0/b/videos-db-7387d.appspot.com/o/videos%2Fmonkey2.mp4?alt=media&token=33a3fbaa-297c-48bf-965b-901cc8d131a2',
+    videoName: 'Monkey family',
+  },
+];
+
+export default Data;
