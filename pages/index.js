81e82ef262065e4e51a0d013695f7718ac5f27ee
Vitor Matias <vitormatiasdev@gmail.com>
2022-12-24 17:30:48 -0300

    feat: adds theater mode by clicking a button

--- pages/index.js
+++ pages/index.js
@@ -20,13 +20,37 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Wrapper>
+      <Wrapper className='wrapper'>
         <Main>
           <h1>Video Player</h1>
+          <br />
+          <button onClick={toggleTheatreMode}>Modo Teatro</button>
+          <br />
           <VideoPlayer data={isVideo} />
         </Main>
         <VideoSidebar setVideo={setVideo} />
       </Wrapper>
     </>
   );
+}
+
+function toggleTheatreMode() {
+  const video = document.querySelector('video');
+  const wrapper = document.querySelector('.wrapper');
+  const aside = document.querySelector('aside');
+  const isTheatreMode = video.classList.toggle('theatre-mode');
+
+  if (isTheatreMode) {
+    video.style.width = '135%';
+    video.style.height = '135%';
+    wrapper.style.display = 'block';
+    aside.style.marginLeft = '69%';
+    aside.style.marginTop = '-5%';
+  } else {
+    video.style.width = '100%';
+    video.style.height = '100%';
+    wrapper.style.display = 'flex';
+    aside.style.marginLeft = '0';
+    aside.style.marginTop = '0';
+  }
 }
